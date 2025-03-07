41639cfcf2b580e052e960d67a007142df02c000
Dan <Daniel-J-Price.dev@pm.me>
2025-03-07 12:40:29 +0000

    feat: allow crud actions via controllers

---
+++ Films.API/Controllers/FilmsController.cs
@@ -0,0 +1,73 @@
+using Films.API.Mapping;
+using Films.Application.Interfaces;
+using Films.Contracts.Requests;
+using Films.Contracts.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Films.API.Controllers;
+
+[ApiController]
+public class FilmsController : ControllerBase
+{
+    private readonly IFilmRepository _filmRepository;
+
+    public FilmsController(IFilmRepository filmRepository)
+    {
+        _filmRepository = filmRepository;
+    }
+
+    [HttpPost(ApiEndpoints.Films.Create)]
+    public async Task<IActionResult> Create([FromBody] CreateFilmRequest request)
+    {
+        var film = request.MapToFilm();
+        await _filmRepository.CreateAsync(film);
+        
+        return CreatedAtAction(nameof(Get), new { id = film.Id }, film);
+    }
+
+    [HttpGet(ApiEndpoints.Films.Get)]
+    public async Task<IActionResult> Get([FromRoute] Guid id)
+    {
+        var film = await _filmRepository.GetByIdAsync(id);
+        if (film is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(film.MapToResponse());
+    }
+    
+    [HttpGet(ApiEndpoints.Films.GetAll)]
+    public async Task<IActionResult> GetAll()
+    {
+        var films = await _filmRepository.GetAllAsync();
+
+        return Ok(films.MapToResponse());
+    }
+
+    [HttpPut(ApiEndpoints.Films.Update)]
+    public async Task<IActionResult> Update([FromRoute]Guid id, [FromBody] UpdateFilmRequest request)
+    {
+        var film = request.MapToFilm(id);
+        var updated = await _filmRepository.UpdateAsync(film);
+        if (!updated)
+        {
+            return NotFound();
+        }
+        
+        var response = film.MapToResponse();
+        return Ok(response);
+    }
+
+    [HttpDelete(ApiEndpoints.Films.Delete)]
+    public async Task<IActionResult> Delete([FromRoute] Guid id)
+    {
+        var deleted = await _filmRepository.DeleteAsync(id);
+        if (!deleted)
+        {
+            return NotFound();
+        }
+
+        return Ok();
+    }
+}
