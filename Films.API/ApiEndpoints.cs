60684699032bcfe5bb7d1ee4633f2538a0a2bfb0
Dan <Daniel-J-Price.dev@pm.me>
2025-03-07 12:37:34 +0000

    chore: create api endpoints in static class

---
+++ Films.API/ApiEndpoints.cs
@@ -0,0 +1,17 @@
+namespace Films.API;
+
+public static class ApiEndpoints
+{
+    private const string ApiBase = "api";
+
+    public static class Films
+    {
+        public const string Base = $"{ApiBase}/films";
+        
+        public const string Create = Base;
+        public const string Get = $"{Base}/{{id:guid}}";
+        public const string GetAll = Base;
+        public const string Update = $"{Base}/{{id:guid}}";
+        public const string Delete = $"{Base}/{{id:guid}}";
+    }
+}
