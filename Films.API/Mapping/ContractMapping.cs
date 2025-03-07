b43b5fd76ac889d189fefcbc6d2829c419adcf62
Dan <Daniel-J-Price.dev@pm.me>
2025-03-07 12:38:10 +0000

    chore: create contract mapping in static class

---
+++ Films.API/Mapping/ContractMapping.cs
@@ -0,0 +1,49 @@
+using Films.Application.Models;
+using Films.Contracts.Requests;
+using Films.Contracts.Responses;
+
+namespace Films.API.Mapping;
+
+public static class ContractMapping
+{
+    public static Film MapToFilm(this CreateFilmRequest request)
+    {
+        return new Film
+        {
+            Id = Guid.NewGuid(),
+            Title = request.Title,
+            YearOfRelease = request.YearOfRelease,
+            Genres = request.Genres.ToList(),
+        };
+    }
+
+    public static FilmResponse MapToResponse(this Film film)
+    {
+        return new FilmResponse
+        {
+            Id = film.Id,
+            Title = film.Title,
+            YearOfRelease = film.YearOfRelease,
+            Genres = film.Genres,
+        };
+    }
+    
+    public static FilmsResponse MapToResponse(this IEnumerable<Film> films)
+    {
+        return new FilmsResponse
+        {
+            Items = films.Select(MapToResponse)
+        };
+    }
+    
+    public static Film MapToFilm(this UpdateFilmRequest request, Guid id)
+    {
+        return new Film
+        {
+            Id = id,
+            Title = request.Title,
+            YearOfRelease = request.YearOfRelease,
+            Genres = request.Genres.ToList(),
+        };
+    }
+}
