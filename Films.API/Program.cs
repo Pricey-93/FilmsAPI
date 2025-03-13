a700abc1d2dce86309fe929f2624be1c63fa4f0b
Dan <Daniel-J-Price.dev@pm.me>
2025-03-13 01:21:03 +0000

    feat: implement postgres database

--- Films.API/Program.cs
+++ Films.API/Program.cs
@@ -1,13 +1,14 @@
 using Scalar.AspNetCore;
 using Films.Application;
-
+using Films.Application.Database;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
+//register services
 builder.Services.AddControllers();
-
 builder.Services.AddApplicationServices();
-
+builder.Services.AddDatabase(config["Database:FilmsConnectionString"]!);
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -24,4 +25,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+//start database
+var dbInitializer = app.Services.GetRequiredService<DbInitializer>();
+await dbInitializer.InitializeAsync();
 app.Run();
---
+++ Films.Application/Database/DbInitializer.cs
@@ -0,0 +1,38 @@
+using Dapper;
+
+namespace Films.Application.Database;
+
+public class DbInitializer
+{
+    private readonly IDbConnectionFactory _dbConnectionFactory;
+
+    public DbInitializer(IDbConnectionFactory dbConnectionFactory)
+    {
+        _dbConnectionFactory = dbConnectionFactory;
+    }
+
+    public async Task InitializeAsync()
+    {
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+
+        await connection.ExecuteAsync("""
+                                      create table if not exists film (
+                                          id UUID primary key,
+                                          slug TEXT not null,
+                                          title TEXT not null,
+                                          year_of_release integer not null);
+                                      """);
+        
+        await connection.ExecuteAsync("""
+                                      create unique index concurrently if not exists film_slug_idx
+                                      on film
+                                      using btree(slug);
+                                      """);
+
+        await connection.ExecuteAsync("""
+                                      create table if not exists genre (
+                                          film_id UUID references film(id),
+                                          name TEXT not null);
+                                      """);
+    }
+}
