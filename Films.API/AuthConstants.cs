d2b8b29cb4fb5a364f15a28e27a4da26e4ac0108
Dan <Daniel-J-Price.dev@pm.me>
2025-03-22 23:20:14 +0000

    feat: configure JWT validation options and policies

---
+++ Films.API/AuthConstants.cs
@@ -0,0 +1,10 @@
+namespace Films.API;
+
+public static class AuthConstants
+{
+    public const string AdminUserPolicyName = "Admin";
+    public const string AdminUserClaimName = "admin";
+    
+    public const string TrustedMemberPolicyName = "Trusted";
+    public const string TrustedMemberClaimName = "trusted_member";
+}
--- Films.API/Program.cs
+++ Films.API/Program.cs
@@ -1,12 +1,48 @@
+using System.Text;
+using Films.API;
 using Scalar.AspNetCore;
 using Films.API.Mapping;
 using Films.Application;
 using Films.Application.Database;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 
 //register services
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters()
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]!)),//store this somewhere safer
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true,
+        ValidIssuer = config["Jwt:Issuer"],
+        ValidAudience = config["Jwt:Audience"],
+        ValidateIssuer = true,
+        ValidateAudience = true,
+    };
+});
+builder.Services.AddAuthorization(x =>
+{
+    x.AddPolicy(AuthConstants.AdminUserPolicyName, 
+        policy => policy.RequireClaim(AuthConstants.AdminUserClaimName, "true"));
+    
+    x.AddPolicy(AuthConstants.TrustedMemberPolicyName,
+        policy => policy.RequireAssertion(
+            context => context.User.HasClaim(match => match is {Type: AuthConstants.AdminUserClaimName, Value: "true" }) || 
+                       context.User.HasClaim(match => match is {Type: AuthConstants.TrustedMemberClaimName, Value: "true" })
+                       )
+        );
+});
+
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices();
 builder.Services.AddDatabase(config["Database:FilmsConnectionString"]!);
@@ -22,6 +58,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.UseMiddleware<ValidationMappingMiddleware>();
